c54cd92f6aa33f6f515c8a9f5c596c1e28b5f476
Dorsa Peikani <Dorsapeikani12345@gmail.com>
2024-07-13 13:33:24 -0700

    adding functionality to buttons

--- script.js
+++ script.js
@@ -6,17 +6,37 @@ class calculator {
   }
 
   clear() {
-    this.previousOperandTextElement = "";
-    this.currentOperandTextElement = "";
+    this.previousOperand = "";
+    this.currentOperand = "";
     this.operation = undefined;
   }
 
   delete() {}
 
-  appendNumber(number) {}
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes("."))
+      //append '.' once!
+      return;
 
-  chooseOperation(operation) {}
-  compute() {}
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === " ") return; //when starting with just the operation symbol
+    if (this.previousOperand !== " ") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = " ";
+  }
+  compute() {
+    this.previousOperand += this.currentOperand;
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.previousOperandTextElement.innerText = this.previousOperand;
+  }
 }
 
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -30,3 +50,22 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
+
+const newCal = new calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    newCal.appendNumber(button.innerText);
+    newCal.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    newCal.chooseOperation(button.innerText);
+    newCal.updateDisplay();
+  });
+});
