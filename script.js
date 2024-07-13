bc96fd8c36d5da5fbd0ac89517defd261d33ca38
Dorsa Peikani <Dorsapeikani12345@gmail.com>
2024-07-13 10:25:52 -0700

    modifying the output css style

---
+++ script.js
@@ -0,0 +1,32 @@
+class calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.previousOperandTextElement = "";
+    this.currentOperandTextElement = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {}
+
+  chooseOperation(operation) {}
+  compute() {}
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
